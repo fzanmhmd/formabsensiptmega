057b12eda495145b5223d174f75c78aacec174ea
Muhammad Fauzan <ojan14021@gmail.com>
2026-04-17 15:12:40 +0700

    Update script.js

--- FORMABSENSI/script.js
+++ FORMABSENSI/script.js
@@ -1,3 +1,15 @@
  flatpickr("#tanggal", {
     dateFormat: "d F Y",
   });
+
+flatpickr("#jam", {
+  enableTime: true,
+  noCalendar: true,
+  dateFormat: "H:i",
+  time_24hr: true
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const element = document.getElementById("shift");
+  new Choices(element);
+});
